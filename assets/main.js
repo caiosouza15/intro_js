89bf772af3bc61123085cbae6e96679ecddab8a8
Caio Souza <caiosouza.15@outlook.com>
2020-12-14 16:43:53 -0300

    Alguns comandos js

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+/*var nome = "Caio Souza";
+var idade1 = 24;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome+ " Tem "+ idade + " anos");//
+//alert(idade+idade2);//
+console.log(nome);
+console.log(idade1*idade2);
+console.log (frase.toUpperCase());*/
+
+/*var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva") //adc um elemento ao array//
+lista.pop(""); //Remove o ultimo elemento do array //
+console.log(lista[0])
+console.log(lista.toString()[0] );
+console.log(lista.join(" - ")); */
+
+/*var fruta = {nome:"maçã", cor:"vermelha"}
+console.log(fruta)
+alert(fruta.cor)
+
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"laranja", cor:"verde"}]
+console.log(fruta)
+alert(fruta.cor)*/
+
+/*var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+}*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+}*/
+
+/*var count;
+for (count=0; count<=5; count++ ){
+    alert(count)
+}*/
+
+/*var d = new Date();
+alert(d.getMonth()+1)*/
